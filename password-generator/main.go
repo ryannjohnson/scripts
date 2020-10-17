02bac62f44f6b2b75ae9f6fbd544d71ef4f08884
Ryan N Johnson <ryan@ryannjohnson.com>
2020-10-17 15:51:37 +0000

    Add password generator

---
+++ password-generator/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"crypto/rand"
+	"flag"
+	"fmt"
+	"math/big"
+)
+
+const (
+	letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	numbers = "0123456789"
+	symbols = "!@#$%^&*()-=_+[]{},.<>;':\"/?|\\"
+)
+
+var (
+	includeLetters = flag.Bool("letters", false, fmt.Sprintf("include %s", letters))
+	includeNumbers = flag.Bool("numbers", false, fmt.Sprintf("include %s", numbers))
+	includeSymbols = flag.Bool("symbols", false, fmt.Sprintf("include %s", symbols))
+	length         = flag.Uint("length", 24, "number of characters in password")
+)
+
+func main() {
+	flag.Parse()
+	includeAll := !*includeLetters && !*includeNumbers && !*includeSymbols
+
+	chars := ""
+	if *includeLetters || includeAll {
+		chars += letters
+	}
+	if *includeNumbers || includeAll {
+		chars += numbers
+	}
+	if *includeSymbols || includeAll {
+		chars += symbols
+	}
+
+	password := make([]byte, *length)
+	for i := range password {
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
+		if err != nil {
+			panic(err)
+		}
+		password[i] = chars[n.Int64()]
+	}
+
+	fmt.Println(string(password[:]))
+}
